b22557b4642e2e6fd77cc6ae2fccb2a29a288d2d
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2019-07-15 00:00:21 +0100

    Update config for local support

--- config.py
+++ config.py
@@ -16,8 +16,8 @@ class Development(Config):
     """Config used in development."""
 
     DEBUG = True
-    PG_PASSWORD = 'postgres'
-    PG_HOST = 'db'
+    PG_PASSWORD = 'my_password'
+    PG_HOST = 'localhost:5433'
     PG_USER = 'postgres'
     SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}/{}'.format(
         PG_USER, PG_PASSWORD, PG_HOST, DATABASE_NAME)
@@ -33,10 +33,12 @@ class Testing(Config):
 
 
 class Minikube(Config):
+    """Config for Minikube."""
+
     DEBUG = True
-    PG_PASSWORD = os.environ.get('PG_PASSWORD', 'postgres')
-    PG_HOST = 'killer-gibbon-postgresql'
-    PG_USER = 'postgres'
+    PG_PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'postgres')
+    PG_HOST = os.environ.get('POSTGRES_HOST', 'killer-gibbon-postgresql')
+    PG_USER = os.environ.get('POSTGRES_USER', 'postgres')
     SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}/{}".format(
         PG_USER, PG_PASSWORD, PG_HOST, DATABASE_NAME)
 
